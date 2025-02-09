e8901ee96efa1b662e0afb6666ccdc65a7049634
bry0306 <37124301+bry0306@users.noreply.github.com>
2025-02-09 17:59:05 -0500

    m

--- components/tabs.tsx
+++ components/tabs.tsx
@@ -282,11 +282,16 @@ const DirectionAwareTabsDemo = ({}) => {
         <div className="bg-zinc-400/15 mt-0.5 text-white/80 w-full flex flex-col items-center p-4 rounded-b-3xl gap-3 outline outline-1 outline-[rgb(132,151,197,0.01)] outline-offset-[-1px] shadow-sm h-20 border-t-3 border-zinc-950">
           
         </div>
-        <div className="bg-zinc-400/15 mt-1 text-white/80 w-full flex flex-col items-center p-4 rounded-3xl gap-3 outline outline-1 outline-[rgb(132,151,197,0.01)] outline-offset-[-1px] shadow-sm h-20 border-t-3 border-zinc-950">
+        <div className="bg-zinc-400/15 mt-1 text-white/80 w-full relative flex flex-col items-center p-4 rounded-3xl gap-3 outline outline-1 outline-[rgb(132,151,197,0.01)] outline-offset-[-1px] shadow-sm h-20 border-t-3 border-zinc-950">
+        <div className="relative flex w-full items-start justify-start">
+            <span className="text-gray/80 text-sm font-medium absolute left-1">
+                To
+            </span>
+        </div>
         <Input
               placeholder="wallet address or SNS name"
               value={recipient}
-              className="w-full h-full file:text-base placeholder:text-base text-base focus-visible:text-base disabled:text-base focus:text-base font-medium file:font-medium placeholder:font-medium disabled:font-medium focus-visible:font-medium focus:font-medium"
+              className="flex items-start justify-start w-full h-full file:text-lg placeholder:text-lg text-lg focus-visible:text-lg disabled:text-lg focus:text-lg font-medium file:font-medium placeholder:font-medium disabled:font-medium focus-visible:font-medium focus:font-medium"
               onChange={(e) => setRecipient(e.target.value)}
               disabled={loading}
             />
